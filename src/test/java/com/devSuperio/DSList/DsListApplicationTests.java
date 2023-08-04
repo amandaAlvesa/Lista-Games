285aba5724f337456e4a502aacf44cc4e6a4f050
Amanda Alves <amandaalves51030@gmail.com>
2023-08-04 16:39:21 -0300

    first commit

---
+++ src/test/java/com/devSuperio/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperio.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
