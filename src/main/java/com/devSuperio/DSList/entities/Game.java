17e90b6909a24540866895ffa3eac49808042717
Amanda Alves <amandaalves51030@gmail.com>
2023-08-07 15:42:13 -0300

    game sedd

--- src/main/java/com/devSuperio/DSList/entities/Game.java
+++ src/main/java/com/devSuperio/DSList/entities/Game.java
@@ -21,10 +21,13 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
@@ -36,7 +39,7 @@ public class Game {
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataforms = plataforms;
+		this.platforms = plataforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -68,10 +71,10 @@ public class Game {
 		this.genre = genre;
 	}
 	public String getPlataforms() {
-		return plataforms;
+		return platforms;
 	}
 	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+		this.platforms = plataforms;
 	}
 	public Double getScore() {
 		return score;
